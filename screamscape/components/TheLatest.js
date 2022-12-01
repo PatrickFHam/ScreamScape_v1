22cbc19cd94798523a2b774cf62bd9838d1f2058
Patrick Ham <patrick.f.ham@gmail.com>
2022-12-01 11:22:17 -0500

    reset, attempt connect MongoDB again

--- screamscape/components/TheLatest.js
+++ screamscape/components/TheLatest.js
@@ -1,8 +1,12 @@
 import { Text, Container } from "@nextui-org/react";
 import { useRouter } from "next/router";
 
-export default function TheLatest () {
+export default function TheLatest ({ news }) {
+
     return(
         <Container><h2>The Latest will go here.</h2></Container>
+
     )
+
+    
 };
